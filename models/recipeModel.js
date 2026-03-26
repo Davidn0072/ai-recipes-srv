8e7748ac7334eb842495ed2140a3845c4ec949fa
Davidn0072 <Davidn0072@Gmail.COM>
2026-03-26 13:40:44 +0200

    add models

---
+++ models/recipeModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const recipeSchema = new mongoose.Schema({
+    title: String,                
+    ingredients: [String],        
+    instructions: String,         
+    difficulty: {                 
+        type: String,
+        enum: ['easy', 'medium', 'hard'],  
+        default: 'easy'
+    },
+    cooking_time: {               
+        type: Number,
+        default: 0
+    }
+  { versionKey: false },	
+});
+
+const Recipe = mongoose.model('recipe', recipeSchema, 'recipes');
+
+export default Recipe;
